e23d22b62c8c993c0880836d84b8f6a1fb4a6c6f
raymond-u <36328498+raymond-u@users.noreply.github.com>
2023-08-31 16:38:01 +0800

    Initial commit

---
+++ run.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+#
+# Set up Nginx and Authelia docker images
+
+# Unofficial bash strict mode
+set -euo pipefail
+
+# Change to the directory of this script
+pushd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null
+
+# Configure Nginx
+nginx_certs_dir=nginx/certs
+nginx_secrets_dir=nginx/secrets
+mkdir -p "${nginx_certs_dir}" "${nginx_secrets_dir}"
+
+if ! [[ -f "${nginx_certs_dir}/self-signed.crt" ]] || ! [[ -f "${nginx_secrets_dir}/self-signed.key" ]]; then
+    openssl req -x509 -nodes -days 36500 -newkey rsa:4096 -sha256 -keyout "${nginx_secrets_dir}/self-signed.key" -out "${nginx_certs_dir}/self-signed.crt"
+fi
+if ! [[ -f "${nginx_certs_dir}/self-signed.pem" ]]; then
+    openssl dhparam -out "${nginx_certs_dir}/self-signed.pem" 4096
+fi
+
+# Configure Authelia
+authelia_db_dir=authelia/db
+authelia_secrets_dir=authelia/secrets
+mkdir -p "${authelia_db_dir}" "${authelia_secrets_dir}"
+
+if ! [[ -f "${authelia_db_dir}/users.yaml" ]]; then
+    touch "${authelia_db_dir}/users.yaml"
+fi
+if ! [[ -f "${authelia_db_dir}/main.sqlite3" ]]; then
+    touch "${authelia_db_dir}/main.sqlite3"
+fi
+
+if ! [[ -f "${authelia_secrets_dir}/jwt_secret" ]]; then
+    tr -dc 'A-Za-z0-9' </dev/urandom | head -c 64 >"${authelia_secrets_dir}/jwt_secret"
+fi
+if ! [[ -f "${authelia_secrets_dir}/notifier_smtp_password" ]]; then
+    read -srp $'\n'"Please enter the value for notifier_smtp_password: " _answer
+    echo "${_answer}" >"${authelia_secrets_dir}/notifier_smtp_password"
+    unset _answer
+fi
+if ! [[ -f "${authelia_secrets_dir}/storage_encryption_key" ]]; then
+    tr -dc 'A-Za-z0-9' </dev/urandom | head -c 64 >"${authelia_secrets_dir}/storage_encryption_key"
+fi
+
+# Configure File Browser
+filebrowser_db_dir=filebrowser/db
+mkdir -p "${filebrowser_db_dir}"
+
+if ! [[ -f "${filebrowser_db_dir}/main.db" ]]; then
+    touch "${filebrowser_db_dir}/main.db"
+fi
+
+# Build docker images
+podman-compose up
