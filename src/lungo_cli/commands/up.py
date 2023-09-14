49e0b2ffe791c5bf8ccab62ac11e542ad4304acb
raymond-u <36328498+raymond-u@users.noreply.github.com>
2023-09-14 11:15:36 +0800

    Update

--- src/lungo_cli/commands/up.py
+++ src/lungo_cli/commands/up.py
@@ -1,11 +1,12 @@
+from pathlib import Path
 from typing import Annotated
 
-from typer import Option
+from typer import Exit, Option
 
-from ..app.state import console, container
+from ..app.state import console, container, users_file
 from ..core.constants import APP_NAME
 from ..helpers.app import check_prerequisites, handle_common_args
-from ..helpers.common import format_command
+from ..helpers.common import format_command, format_input, format_path
 
 
 def main(
@@ -18,6 +19,29 @@ def main(
     """
     handle_common_args(quiet)
     check_prerequisites()
+
+    # Check if the shared directories and user home directories exist
+    shared_dir = Path.home().parent.joinpath("shared")
+    shared_readonly_dir = Path.home().parent.joinpath("shared_readonly")
+
+    if not shared_dir.exists() or not shared_readonly_dir.exists():
+        console().print_error(
+            f"The {format_path('shared')} and {format_path('shared_readonly')} directories are missing."
+            "Please create them and configure their permissions as described in the documentation."
+        )
+        raise Exit(code=1)
+
+    users = users_file().load()
+
+    for user in users:
+        user_home_dir = Path.home().parent.joinpath(user.username)
+
+        if not user_home_dir.exists():
+            console().print_error(
+                f"The home directory for user {format_input(user.username)} is missing."
+                "Please create the user and configure its permissions as described in the documentation."
+            )
+            raise Exit(code=1)
 
     # Start the service
     container().up()
