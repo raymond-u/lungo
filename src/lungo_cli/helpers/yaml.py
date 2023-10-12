1861a581909bb714bba37f769e25a7902ec71f6f
raymond-u <36328498+raymond-u@users.noreply.github.com>
2023-10-12 20:08:47 +0800

    Update

--- src/lungo_cli/helpers/yaml.py
+++ src/lungo_cli/helpers/yaml.py
@@ -1,7 +1,7 @@
 from os import PathLike
 from typing import Type, TypeVar
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 from pydantic_yaml import parse_yaml_file_as
 from typer import Exit
 
@@ -15,6 +15,22 @@ def parse_yaml(path: str | PathLike[str], model: Type[T]) -> T:
     """Parse a YAML file as a Pydantic model."""
     try:
         return parse_yaml_file_as(model, path)
+    except ValidationError as e:
+        error_msg = f"Failed to parse {format_path(path)}:"
+
+        for error in e.errors():
+            error_msg += "\n  "
+
+            for loc in error.loc():
+                if type(loc) is int:
+                    error_msg += f"[{loc}]"
+                else:
+                    error_msg += f".{loc}"
+
+            error_msg += f": {error.msg}"
+
+        console().print_error(error_msg)
+        raise Exit(code=1)
     except Exception as e:
         console().print_error(f"Failed to parse {format_path(path)} ({e}).")
         raise Exit(code=1)
