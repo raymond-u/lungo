f8ebbd8eedace8f47adb422a8ba3af697c9809e6
raymond-u <36328498+raymond-u@users.noreply.github.com>
2023-10-16 22:59:45 +0800

    Update

--- src/lungo_cli/resources/web/src/routes/(apps)/app/+layout.svelte
+++ src/lungo_cli/resources/web/src/routes/(apps)/app/+layout.svelte
@@ -1,28 +1,57 @@
 <script lang="ts">
+    import { onMount } from "svelte"
     import { goto } from "$app/navigation"
     import { page } from "$app/stores"
     import { useStore } from "$lib/utils"
 
     const { currentApp } = useStore()
-    const src = $page.url.pathname + "?iframe=1"
 
     let iframe: HTMLIFrameElement | undefined
 
-    let oldHref: string | undefined = ""
-    let oldReferer: string | undefined = ""
+    onMount(() => {
+        Object.defineProperty(iframe!.contentWindow!.history, "pushState", {
+            value: (
+                data: Parameters<typeof history.pushState>[0],
+                unused: Parameters<typeof history.pushState>[1],
+                url: Parameters<typeof history.pushState>[2] = undefined
+            ) => {
+                if (url) {
+                    let newUrl = new URL(url)
 
-    setInterval(() => {
-        if (iframe?.contentWindow?.location.href === oldHref && iframe?.contentDocument?.referrer === oldReferer) {
-            return
-        }
+                    if (!newUrl.searchParams.has("iframe", "1")) {
+                        newUrl.searchParams.set("iframe", "1")
+                    }
 
-        oldHref = iframe?.contentWindow?.location.href
-        oldReferer = iframe?.contentDocument?.referrer
+                    iframe!.contentWindow!.history.pushState(data, unused, newUrl)
+                } else {
+                    iframe!.contentWindow!.history.pushState(data, unused)
+                }
+            },
+            writable: false,
+        })
+        Object.defineProperty(iframe!.contentWindow!.history, "replaceState", {
+            value: (
+                data: Parameters<typeof history.replaceState>[0],
+                unused: Parameters<typeof history.replaceState>[1],
+                url: Parameters<typeof history.replaceState>[2] = undefined
+            ) => {
+                if (url) {
+                    let newUrl = new URL(url)
 
-        console.log("###")
-        console.log("Href changed to: " + oldHref)
-        console.log("Referer changed to: " + oldReferer)
-    }, 1)
+                    if (!newUrl.searchParams.has("iframe", "1")) {
+                        newUrl.searchParams.set("iframe", "1")
+                    }
+
+                    iframe!.contentWindow!.history.replaceState(data, unused, newUrl)
+                } else {
+                    iframe!.contentWindow!.history.replaceState(data, unused)
+                }
+            },
+            writable: false,
+        })
+
+        iframe!.src = $page.url.pathname + "?iframe=1"
+    })
 
     // $: if (iframe && iframe.contentWindow) {
     //     goto(iframe.contentWindow.location.pathname, { replaceState: true })
@@ -31,6 +60,6 @@
 
 <div class="relative flex-1 overflow-y-auto px-6 py-12">
     {#if $currentApp}
-        <iframe {src} title={$currentApp.name} class="h-full w-full" bind:this={iframe} />
+        <iframe title={$currentApp.name} class="h-full w-full" bind:this={iframe} />
     {/if}
 </div>
