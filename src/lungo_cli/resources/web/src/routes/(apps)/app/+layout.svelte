c42c0fe35ba58d9bda51d24e070acdd3de79b73b
raymond-u <36328498+raymond-u@users.noreply.github.com>
2023-10-16 18:35:33 +0800

    Update

--- src/lungo_cli/resources/web/src/routes/(apps)/app/+layout.svelte
+++ src/lungo_cli/resources/web/src/routes/(apps)/app/+layout.svelte
@@ -8,12 +8,19 @@
 
     let iframe: HTMLIFrameElement | undefined
 
+    $: {
+        console.log("###")
+        console.log("Href changed to: " + iframe?.contentWindow?.location.href)
+        console.log("Referer changed to: " + iframe?.contentDocument?.referrer)
+        console.log("History changed to: " + iframe?.contentWindow?.history.state)
+    }
+
     // $: if (iframe && iframe.contentWindow) {
     //     goto(iframe.contentWindow.location.pathname, { replaceState: true })
     // }
 </script>
 
-<div class="flex-1 overflow-y-auto px-6 py-12">
+<div class="relative flex-1 overflow-y-auto px-6 py-12">
     {#if $currentApp}
         <iframe {src} title={$currentApp.name} class="h-full w-full" bind:this={iframe} />
     {/if}
