9ee10beb20a0304240bff4ffe9c11737adc94511
raymond-u <36328498+raymond-u@users.noreply.github.com>
2023-10-17 15:04:28 +0800

    Update

--- src/lungo_cli/resources/web/src/routes/(apps)/app/+layout.svelte
+++ src/lungo_cli/resources/web/src/routes/(apps)/app/+layout.svelte
@@ -12,6 +12,14 @@
         console.log("iframe loaded with url: ", iframe.contentWindow!.location.href)
         console.log("original history pushState(): ", iframe.contentWindow!.history.pushState)
 
+        iframe.contentWindow!.history.pushState = () => {
+            History.prototype.pushState.apply(iframe.contentWindow!.history, arguments)
+        }
+
+        iframe.contentWindow!.history.replaceState = () => {
+            History.prototype.replaceState.apply(iframe.contentWindow!.history, arguments)
+        }
+
         // iframe.contentWindow!.addEventListener("popstate", () => {
         //     console.log("popstate called with url: ", iframe.contentWindow!.location.href)
         //
@@ -23,45 +31,46 @@
         //         iframe.contentWindow!.history.replaceState(iframe.contentWindow!.history.state, "", url)
         //     }
         // })
-        iframe.contentWindow!.history.pushState = (
-            data: Parameters<typeof history.pushState>[0],
-            unused: Parameters<typeof history.pushState>[1],
-            url: Parameters<typeof history.pushState>[2] = undefined
-        ) => {
-            console.log("pushState called with url: ", url)
 
-            if (url) {
-                const newUrl = new URL(url)
-
-                if (!newUrl.searchParams.has("iframe", "1")) {
-                    newUrl.searchParams.set("iframe", "1")
-                }
-
-                iframe.contentWindow!.history.pushState(data, unused, newUrl)
-            } else {
-                iframe.contentWindow!.history.pushState(data, unused)
-            }
-        }
-
-        iframe.contentWindow!.history.replaceState = (
-            data: Parameters<typeof history.replaceState>[0],
-            unused: Parameters<typeof history.replaceState>[1],
-            url: Parameters<typeof history.replaceState>[2] = undefined
-        ) => {
-            console.log("replaceState called with url: ", url)
-
-            if (url) {
-                const newUrl = new URL(url)
-
-                if (!newUrl.searchParams.has("iframe", "1")) {
-                    newUrl.searchParams.set("iframe", "1")
-                }
-
-                iframe.contentWindow!.history.replaceState(data, unused, newUrl)
-            } else {
-                iframe.contentWindow!.history.replaceState(data, unused)
-            }
-        }
+        // iframe.contentWindow!.history.pushState = (
+        //     data: Parameters<typeof history.pushState>[0],
+        //     unused: Parameters<typeof history.pushState>[1],
+        //     url: Parameters<typeof history.pushState>[2] = undefined
+        // ) => {
+        //     console.log("pushState called with url: ", url)
+        //
+        //     if (url) {
+        //         const newUrl = new URL(url)
+        //
+        //         if (!newUrl.searchParams.has("iframe", "1")) {
+        //             newUrl.searchParams.set("iframe", "1")
+        //         }
+        //
+        //         iframe.contentWindow!.history.pushState(data, unused, newUrl)
+        //     } else {
+        //         iframe.contentWindow!.history.pushState(data, unused)
+        //     }
+        // }
+        //
+        // iframe.contentWindow!.history.replaceState = (
+        //     data: Parameters<typeof history.replaceState>[0],
+        //     unused: Parameters<typeof history.replaceState>[1],
+        //     url: Parameters<typeof history.replaceState>[2] = undefined
+        // ) => {
+        //     console.log("replaceState called with url: ", url)
+        //
+        //     if (url) {
+        //         const newUrl = new URL(url)
+        //
+        //         if (!newUrl.searchParams.has("iframe", "1")) {
+        //             newUrl.searchParams.set("iframe", "1")
+        //         }
+        //
+        //         iframe.contentWindow!.history.replaceState(data, unused, newUrl)
+        //     } else {
+        //         iframe.contentWindow!.history.replaceState(data, unused)
+        //     }
+        // }
 
         // Object.defineProperty(iframe.contentWindow!.history, "pushState", {
         //     value: (
