87ffeb85a01ff57fe48ac80e5e8a0296bc3883a7
raymond-u <36328498+raymond-u@users.noreply.github.com>
2023-10-18 04:13:05 +0800

    Update

--- src/lungo_cli/resources/web/src/routes/(apps)/app/rstudio/+layout.server.ts
+++ src/lungo_cli/resources/web/src/routes/(apps)/app/rstudio/+layout.server.ts
@@ -17,50 +17,50 @@ export async function load({
 }) {
     const wrappedFetch = wrapFetch(fetch, cookies)
 
-    try {
-        const response = await wrappedFetch("/app/rstudio?iframe=1", { redirect: "manual" })
-
-        // If the user is already logged in, return
-        if (response.ok) {
-            return {
-                title: "R Studio",
-            }
-        }
-
-        const { userInfo } = await parent()
-
-        const response2 = await wrappedFetch("/app/rstudio/js/encrypt.min.js?iframe=1")
-        eval(await response2.text())
-
-        const username = userInfo?.traits?.username ?? "anonymous"
-        const password = "passwd"
-        const payload = `${username}\n${password}`
-
-        const response3 = await wrappedFetch("/app/rstudio/auth-sign-in?iframe=1")
-        const text = await response3.text()
-
-        const html = new DOMParser().parseFromString(text, "text/html")
-        const key = html.getElementsByName("public-key-url")[0].getAttribute("content")
-
-        const response4 = await wrappedFetch(`/app/rstudio/${key}?iframe=1`)
-        const text2 = await response4.text()
-        const [exp, mod] = text2.split(":", 2)
-
-        ;(html.getElementById("clientPath") as HTMLInputElement).value = "/app/rstudio/auth-sign-in"
-        ;(html.getElementById("package") as HTMLInputElement).value = encrypt(payload, exp, mod)
-        ;(html.getElementById("persist") as HTMLInputElement).value = "0"
-
-        await wrappedFetch("/app/rstudio/auth-do-sign-in?iframe=1", {
-            method: "POST",
-            redirect: "manual",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: asSearchParams(html.getElementsByName("realform")[0] as HTMLFormElement),
-        })
-    } catch (e) {
-        throw error(500, "Failed to connect to server.")
-    }
+    // try {
+    //     const response = await wrappedFetch("/app/rstudio?iframe=1", { redirect: "manual" })
+    //
+    //     // If the user is already logged in, return
+    //     if (response.ok) {
+    //         return {
+    //             title: "R Studio",
+    //         }
+    //     }
+    //
+    //     const { userInfo } = await parent()
+    //
+    //     const response2 = await wrappedFetch("/app/rstudio/js/encrypt.min.js?iframe=1")
+    //     eval(await response2.text())
+    //
+    //     const username = userInfo?.traits?.username ?? "anonymous"
+    //     const password = "passwd"
+    //     const payload = `${username}\n${password}`
+    //
+    //     const response3 = await wrappedFetch("/app/rstudio/auth-sign-in?iframe=1")
+    //     const text = await response3.text()
+    //
+    //     const html = new DOMParser().parseFromString(text, "text/html")
+    //     const key = html.getElementsByName("public-key-url")[0].getAttribute("content")
+    //
+    //     const response4 = await wrappedFetch(`/app/rstudio/${key}?iframe=1`)
+    //     const text2 = await response4.text()
+    //     const [exp, mod] = text2.split(":", 2)
+    //
+    //     ;(html.getElementById("clientPath") as HTMLInputElement).value = "/app/rstudio/auth-sign-in"
+    //     ;(html.getElementById("package") as HTMLInputElement).value = encrypt(payload, exp, mod)
+    //     ;(html.getElementById("persist") as HTMLInputElement).value = "0"
+    //
+    //     await wrappedFetch("/app/rstudio/auth-do-sign-in?iframe=1", {
+    //         method: "POST",
+    //         redirect: "manual",
+    //         headers: {
+    //             "Content-Type": "application/x-www-form-urlencoded",
+    //         },
+    //         body: asSearchParams(html.getElementsByName("realform")[0] as HTMLFormElement),
+    //     })
+    // } catch (e) {
+    //     throw error(500, "Failed to connect to server.")
+    // }
 
     return {
         title: "R Studio",
