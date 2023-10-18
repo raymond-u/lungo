57e776867c28af13f381a72bde3ca60c946096fc
raymond-u <36328498+raymond-u@users.noreply.github.com>
2023-10-19 05:47:18 +0800

    Update

--- src/lungo_cli/resources/web/src/routes/(apps)/app/rstudio/+layout.server.ts
+++ src/lungo_cli/resources/web/src/routes/(apps)/app/rstudio/+layout.server.ts
@@ -32,7 +32,9 @@ export async function load({
 
     console.log("##### get 1 #####")
 
-    const response = await wrappedFetch("/app/rstudio/auth-sign-in?iframe=1", { redirect: "manual" })
+    const response = await wrappedFetch("https://172.16.39.102/app/rstudio/auth-sign-in?iframe=1", {
+        redirect: "manual",
+    })
 
     // If the user is already logged in, return
     if (response.status == 302) {
@@ -48,7 +50,7 @@ export async function load({
 
     console.log("##### get 3 #####")
 
-    const response2 = await wrappedFetch(`/app/rstudio/${key}?iframe=1`)
+    const response2 = await wrappedFetch(`https://172.16.39.102/app/rstudio/${key}?iframe=1`)
     const text2 = await response2.text()
     const [exp, mod] = text2.split(":", 2)
 
@@ -57,7 +59,7 @@ export async function load({
     // const html = new DOMParser().parseFromString(await response.text(), "text/html")
     // const key = html.getElementsByName("public-key-url")[0].getAttribute("content")
 
-    const response3 = await wrappedFetch("/app/rstudio/js/encrypt.min.js?iframe=1")
+    const response3 = await wrappedFetch("https://172.16.39.102/app/rstudio/js/encrypt.min.js?iframe=1")
     const text = await response3.text()
     const encrypt = new Function(
         "payload",
@@ -84,7 +86,7 @@ export async function load({
     // ;(html.getElementById("package") as HTMLInputElement).value = encrypt(payload, exp, mod)
     // ;(html.getElementById("persist") as HTMLInputElement).value = "0"
 
-    const response4 = await wrappedFetch("/app/rstudio/auth-do-sign-in?iframe=1", {
+    const response4 = await wrappedFetch("https://172.16.39.102/app/rstudio/auth-do-sign-in?iframe=1", {
         method: "POST",
         redirect: "manual",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
