c3a560897c2fc8cfd24cbc6cfb60dc78f1588c7c
raymond-u <36328498+raymond-u@users.noreply.github.com>
2023-10-18 23:35:54 +0800

    Update

--- src/lungo_cli/resources/web/src/lib/server/constants.ts
+++ src/lungo_cli/resources/web/src/lib/server/constants.ts
@@ -2,3 +2,4 @@ import { env } from "$env/dynamic/private"
 
 export const KETO_API_BASE_URL = env.KETO_API_BASE_URL
 export const KRATOS_API_BASE_URL = env.KRATOS_API_BASE_URL
+export const RSTUDIO_BASE_URL = env.RSTUDIO_BASE_URL
--- src/lungo_cli/resources/web/src/routes/(apps)/app/rstudio/+layout.server.ts
+++ src/lungo_cli/resources/web/src/routes/(apps)/app/rstudio/+layout.server.ts
@@ -1,9 +1,9 @@
-import type { Cookies } from "@sveltejs/kit"
-import * as cheerio from "cheerio"
+import { load as loadHtmlString } from "cheerio"
+import { type Cookies, error } from "@sveltejs/kit"
+import { wrapFetch } from "$lib/server/api"
+import { RSTUDIO_BASE_URL } from "$lib/server/constants"
 import type { User } from "$lib/types"
 // import { asSearchParams } from "$lib/utils"
-import { wrapFetch } from "$lib/server/api"
-import { error } from "@sveltejs/kit"
 
 export async function load({
     cookies,
@@ -17,7 +17,9 @@ export async function load({
     const wrappedFetch = wrapFetch(fetch, cookies)
 
     try {
-        const response = await wrappedFetch("/app/rstudio/auth-sign-in?iframe=1", { redirect: "manual" })
+        console.log("##### get 1 #####")
+
+        const response = await wrappedFetch(`${RSTUDIO_BASE_URL}auth-sign-in`, { redirect: "manual" })
 
         // If the user is already logged in, return
         if (response.status === 302) {
@@ -26,15 +28,21 @@ export async function load({
             }
         }
 
-        const $ = cheerio.load(await response.text())
+        console.log("##### get 2 #####")
+
+        const $ = loadHtmlString(await response.text())
         const key = $("meta[name=public-key-url]").attr("content")!
+
+        console.log("##### get 3 #####")
 
         // const html = new DOMParser().parseFromString(await response.text(), "text/html")
         // const key = html.getElementsByName("public-key-url")[0].getAttribute("content")
 
         const { userInfo } = await parent()
 
-        const response2 = await wrappedFetch("/app/rstudio/js/encrypt.min.js?iframe=1")
+        console.log("##### get 4 #####")
+
+        const response2 = await wrappedFetch(`${RSTUDIO_BASE_URL}js/encrypt.min.js`)
         const text = await response2.text()
         const encrypt = new Function(
             "payload",
@@ -43,20 +51,24 @@ export async function load({
             `"use strict"; ${text.replace("window.encrypt", "var encrypt")}; return encrypt(payload, exp, mod);`
         ) as (payload: string, exp: string, mod: string) => string
 
+        console.log("##### get 5 #####")
+
         const username = userInfo?.traits?.username ?? "anonymous"
         const password = "passwd"
         const payload = `${username}\n${password}`
 
-        const response3 = await wrappedFetch(`/app/rstudio/${key}?iframe=1`)
+        const response3 = await wrappedFetch(`${RSTUDIO_BASE_URL}${key}`)
         const text2 = await response3.text()
         const [exp, mod] = text2.split(":", 2)
+
+        console.log("##### get 6 #####")
 
         const csrf = $("form[name=realform] > input:eq(1)")
         // ;(html.getElementById("clientPath") as HTMLInputElement).value = "/app/rstudio/auth-sign-in"
         // ;(html.getElementById("package") as HTMLInputElement).value = encrypt(payload, exp, mod)
         // ;(html.getElementById("persist") as HTMLInputElement).value = "0"
 
-        await wrappedFetch("/app/rstudio/auth-do-sign-in?iframe=1", {
+        await wrappedFetch(`${RSTUDIO_BASE_URL}auth-do-sign-in`, {
             method: "POST",
             redirect: "manual",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -69,6 +81,8 @@ export async function load({
             }),
             // body: asSearchParams(html.getElementsByName("realform")[0] as HTMLFormElement),
         })
+
+        console.log("##### get 7 #####")
     } catch (e) {
         console.log("####################")
         console.log(e)
@@ -76,6 +90,8 @@ export async function load({
         console.log("####################")
         throw error(500, "Failed to connect to server.")
     }
+
+    console.log("##### get 8 #####")
 
     return {
         title: "R Studio",
