c7febf56411f5bf104f6090b7dfdf790d9462402
raymond-u <36328498+raymond-u@users.noreply.github.com>
2023-10-19 01:09:42 +0800

    Update

--- src/lungo_cli/resources/web/src/lib/server/api.ts
+++ src/lungo_cli/resources/web/src/lib/server/api.ts
@@ -4,9 +4,41 @@ import { type Cookies, error } from "@sveltejs/kit"
 import { KETO_API_BASE_URL, KRATOS_API_BASE_URL } from "$lib/server/constants"
 import type { KetoPaths, KratosPaths } from "$lib/types"
 
-export function wrapFetch(fetch: typeof global.fetch, cookies?: Cookies): typeof global.fetch {
+export function wrapFetch({
+    fetch,
+    baseUrl,
+    cookies,
+    credentials,
+    headers,
+    ensureOk,
+}: {
+    fetch: typeof global.fetch
+    baseUrl?: string
+    cookies?: Cookies
+    credentials?: RequestCredentials
+    headers?: HeadersInit
+    ensureOk?: boolean
+}): typeof global.fetch {
     return async (input, init?) => {
+        let response: Response
+
         try {
+            if (baseUrl) {
+                if (typeof input === "string") {
+                    if (input.match("https?://")) {
+                        const url = new URL(input)
+                        input = new URL(url.pathname + url.search + url.hash, baseUrl)
+                    } else {
+                        input = new URL(input, baseUrl)
+                    }
+                } else if (input instanceof URL) {
+                    input = new URL(input.pathname + input.search + input.hash, baseUrl)
+                } else if (input instanceof Request) {
+                    const url = new URL(input.url)
+                    input = new Request(new URL(url.pathname + url.search + url.hash, baseUrl), input)
+                }
+            }
+
             if (cookies && cookies.getAll().length > 0) {
                 init ??= {}
                 init.headers = new Headers(init.headers)
@@ -19,41 +51,63 @@ export function wrapFetch(fetch: typeof global.fetch, cookies?: Cookies): typeof
                 )
             }
 
+            if (credentials) {
+                init ??= {}
+                init.credentials = credentials
+            }
+
+            if (headers) {
+                init ??= {}
+                init.headers = new Headers(init.headers)
+                for (const [key, value] of Object.entries(headers)) {
+                    init.headers.set(key, value)
+                }
+            }
+
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
-            const response = await fetch(input, init)
+            response = await fetch(input, init)
 
             if (cookies) {
                 for (const cookie of parser.parse(response.headers.getSetCookie())) {
                     cookies.set(cookie.name, cookie.value, {
-                        path: "/",
+                        path: cookie.path?.startsWith("/app") ? cookie.path : "/",
                         expires: cookie.expires,
                         maxAge: cookie.maxAge,
                     })
                 }
             }
-
-            return response
         } catch (e) {
-            throw error(500, "Failed to connect to server.")
+            throw error(500)
         }
+
+        if (ensureOk && !(response.status >= 200 && response.status < 400)) {
+            throw error(500)
+        }
+
+        return response
     }
 }
 
 export const createKetoClient = (fetch: typeof global.fetch) => {
     return createClient<KetoPaths>({
-        baseUrl: KETO_API_BASE_URL,
-        fetch: wrapFetch(fetch),
-        credentials: "include",
-        headers: { Accept: "application/json" },
+        fetch: wrapFetch({
+            fetch,
+            baseUrl: KETO_API_BASE_URL,
+            credentials: "include",
+            headers: { Accept: "application/json" },
+        }),
     })
 }
 
 export const createKratosClient = (cookies: Cookies, fetch: typeof global.fetch) => {
     return createClient<KratosPaths>({
-        baseUrl: KRATOS_API_BASE_URL,
-        fetch: wrapFetch(fetch, cookies),
-        credentials: "include",
-        headers: { Accept: "application/json" },
+        fetch: wrapFetch({
+            fetch,
+            baseUrl: KRATOS_API_BASE_URL,
+            cookies,
+            credentials: "include",
+            headers: { Accept: "application/json" },
+        }),
     })
 }
--- src/lungo_cli/resources/web/src/routes/(apps)/app/rstudio/+layout.server.ts
+++ src/lungo_cli/resources/web/src/routes/(apps)/app/rstudio/+layout.server.ts
@@ -1,5 +1,5 @@
 import { load as loadHtmlString } from "cheerio"
-import { type Cookies, error } from "@sveltejs/kit"
+import type { Cookies } from "@sveltejs/kit"
 import { wrapFetch } from "$lib/server/api"
 import { RSTUDIO_BASE_URL } from "$lib/server/constants"
 import type { User } from "$lib/types"
@@ -14,87 +14,91 @@ export async function load({
     fetch: typeof global.fetch
     parent: () => Promise<{ userInfo: User | undefined }>
 }) {
-    const wrappedFetch = wrapFetch(fetch, cookies)
+    const wrappedFetch = wrapFetch({
+        fetch,
+        baseUrl: RSTUDIO_BASE_URL,
+        cookies,
+        credentials: "include",
+        ensureOk: true,
+    })
 
-    try {
-        console.log("##### get 1 #####")
+    console.log("##### get 1 #####")
 
-        const response = await wrappedFetch(`${RSTUDIO_BASE_URL}auth-sign-in`, { redirect: "manual" })
+    const response = await wrappedFetch("/auth-sign-in", { redirect: "manual" })
 
-        // If the user is already logged in, return
-        if (response.status === 302) {
-            return {
-                title: "R Studio",
-            }
+    // If the user is already logged in, return
+    if (response.status == 302) {
+        return {
+            title: "R Studio",
         }
-
-        console.log("##### get 2 #####")
-
-        const $ = loadHtmlString(await response.text())
-        const key = $("meta[name=public-key-url]").attr("content")!
-
-        console.log("##### get 3 #####")
-
-        // const html = new DOMParser().parseFromString(await response.text(), "text/html")
-        // const key = html.getElementsByName("public-key-url")[0].getAttribute("content")
-
-        const { userInfo } = await parent()
-
-        console.log("##### get 4 #####")
-
-        const response2 = await wrappedFetch(`${RSTUDIO_BASE_URL}js/encrypt.min.js`)
-        const text = await response2.text()
-        const encrypt = new Function(
-            "payload",
-            "exp",
-            "mod",
-            `"use strict";
-            const navigator = { appName: "Netscape", appVersion: "5.0" };
-            ${text.replace("window.encrypt", "var encrypt")};
-            return encrypt(payload, exp, mod);`
-        ) as (payload: string, exp: string, mod: string) => string
-
-        console.log("##### get 5 #####")
-
-        const username = userInfo?.traits?.username ?? "anonymous"
-        const password = "passwd"
-        const payload = `${username}\n${password}`
-
-        const response3 = await wrappedFetch(`${RSTUDIO_BASE_URL}${key}`)
-        const text2 = await response3.text()
-        const [exp, mod] = text2.split(":", 2)
-
-        console.log("##### get 6 #####")
-
-        const csrf = $("form[name=realform] > input:eq(1)")
-        // ;(html.getElementById("clientPath") as HTMLInputElement).value = "/app/rstudio/auth-sign-in"
-        // ;(html.getElementById("package") as HTMLInputElement).value = encrypt(payload, exp, mod)
-        // ;(html.getElementById("persist") as HTMLInputElement).value = "0"
-
-        await wrappedFetch(`${RSTUDIO_BASE_URL}auth-do-sign-in`, {
-            method: "POST",
-            redirect: "manual",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: new URLSearchParams({
-                persist: "0",
-                [csrf.attr("name")!]: csrf.attr("value")!,
-                appUri: "/",
-                clientPath: "/app/rstudio/auth-sign-in",
-                v: encrypt(payload, exp, mod),
-            }),
-            // body: asSearchParams(html.getElementsByName("realform")[0] as HTMLFormElement),
-        })
-
-        console.log("##### get 7 #####")
-    } catch (e) {
-        console.log("####################")
-        console.log(e)
-        console.log(JSON.stringify(e))
-        console.log("####################")
-        throw error(500, "Failed to connect to server.")
     }
 
-    console.log("##### get 8 #####")
+    console.log("##### get 2 #####")
+
+    const $ = loadHtmlString(await response.text())
+    const key = $("meta[name=public-key-url]").attr("content")!
+
+    console.log("##### get 3 #####")
+
+    // const html = new DOMParser().parseFromString(await response.text(), "text/html")
+    // const key = html.getElementsByName("public-key-url")[0].getAttribute("content")
+
+    const { userInfo } = await parent()
+
+    console.log("##### get 4 #####")
+
+    const response2 = await wrappedFetch("/js/encrypt.min.js")
+    const text = await response2.text()
+    const encrypt = new Function(
+        "payload",
+        "exp",
+        "mod",
+        `"use strict";
+        const navigator = { appName: "Netscape", appVersion: "5.0" };
+        ${text.replace("window.encrypt", "var encrypt")};
+        return encrypt(payload, exp, mod);`
+    ) as (payload: string, exp: string, mod: string) => string
+
+    console.log("##### get 5 #####")
+
+    const username = userInfo?.traits?.username ?? "anonymous"
+    const password = "passwd"
+    const payload = `${username}\n${password}`
+
+    console.log(`##### get ${payload} #####`)
+
+    const response3 = await wrappedFetch(`/${key}`)
+    const text2 = await response3.text()
+    const [exp, mod] = text2.split(":", 2)
+
+    console.log("##### get 6 #####")
+    console.log(`##### get ${exp} #####`)
+    console.log(`##### get ${mod} #####`)
+
+    const csrf = $("form[name=realform] > input:eq(1)")
+    // ;(html.getElementById("clientPath") as HTMLInputElement).value = "/app/rstudio/auth-sign-in"
+    // ;(html.getElementById("package") as HTMLInputElement).value = encrypt(payload, exp, mod)
+    // ;(html.getElementById("persist") as HTMLInputElement).value = "0"
+
+    console.log(`##### get ${csrf.attr("name")} #####`)
+    console.log(`##### get ${csrf.attr("value")} #####`)
+    console.log(`##### get ${encrypt(payload, exp, mod)} #####`)
+
+    await wrappedFetch("/auth-do-sign-in", {
+        method: "POST",
+        redirect: "manual",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams({
+            persist: "0",
+            [csrf.attr("name")!]: csrf.attr("value")!,
+            appUri: "/",
+            clientPath: "/app/rstudio/auth-sign-in",
+            v: encrypt(payload, exp, mod),
+        }),
+        // body: asSearchParams(html.getElementsByName("realform")[0] as HTMLFormElement),
+    })
+
+    console.log("##### get 7 #####")
 
     return {
         title: "R Studio",
