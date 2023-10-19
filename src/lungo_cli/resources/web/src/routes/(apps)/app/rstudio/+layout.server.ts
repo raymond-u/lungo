6d706f0ba2744dde06f24b855d990d27f18832f4
raymond-u <36328498+raymond-u@users.noreply.github.com>
2023-10-19 17:20:23 +0800

    Update

--- src/lungo_cli/resources/web/src/routes/(apps)/app/rstudio/+layout.server.ts
+++ src/lungo_cli/resources/web/src/routes/(apps)/app/rstudio/+layout.server.ts
@@ -3,7 +3,6 @@ import type { Cookies } from "@sveltejs/kit"
 import { wrapFetch } from "$lib/server/api"
 import { RSTUDIO_BASE_URL } from "$lib/server/constants"
 import type { User } from "$lib/types"
-import parser from "set-cookie-parser"
 
 export async function load({
     cookies,
@@ -23,19 +22,7 @@ export async function load({
         ensureOk: true,
     })
 
-    // const testFetch = wrapFetch({
-    //     fetch,
-    //     baseUrl: "https://httpbin.org/",
-    //     cookies,
-    //     credentials: "include",
-    //     ensureOk: true,
-    // })
-
-    console.log("##### get 1 #####")
-
-    const response = await wrappedFetch("/auth-sign-in", {
-        redirect: "manual",
-    })
+    const response = await wrappedFetch("/auth-sign-in", { redirect: "manual" })
 
     // If the user is already logged in, return
     if (response.status == 302) {
@@ -44,21 +31,12 @@ export async function load({
         }
     }
 
-    console.log("##### get 2 #####")
-
     const $ = loadHtmlString(await response.text())
     const key = $("meta[name=public-key-url]").attr("content")!
-
-    console.log("##### get 3 #####")
 
     const response2 = await wrappedFetch(`/${key}`)
     const text2 = await response2.text()
     const [exp, mod] = text2.split(":", 2)
-
-    console.log("##### get 4 #####")
-
-    // const html = new DOMParser().parseFromString(await response.text(), "text/html")
-    // const key = html.getElementsByName("public-key-url")[0].getAttribute("content")
 
     const response3 = await wrappedFetch("/js/encrypt.min.js")
     const text = await response3.text()
@@ -72,111 +50,37 @@ export async function load({
         return encrypt(payload, exp, mod);`
     ) as (payload: string, exp: string, mod: string) => string
 
-    console.log("##### get 5 #####")
-
     const { userInfo } = await parent()
 
     const username = userInfo?.username ?? "anonymous"
     const password = "passwd"
     const payload = `${username}\n${password}`
 
-    console.log(`##### get ${payload} #####`)
-
     const csrf = $("form[name=realform] > input:eq(1)")
-    // ;(html.getElementById("clientPath") as HTMLInputElement).value = "/app/rstudio/auth-sign-in"
-    // ;(html.getElementById("package") as HTMLInputElement).value = encrypt(payload, exp, mod)
-    // ;(html.getElementById("persist") as HTMLInputElement).value = "0"
 
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
+    for (;;) {
         const response4 = await wrappedFetch("/auth-do-sign-in", {
             method: "POST",
             redirect: "manual",
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded",
-                "X-RStudio-Root-Path": "/app/rstudio",
-                "X-Forwarded-Proto": "https",
-                "X-Forwarded-Host": "172.16.39.102",
+                // "X-RStudio-Root-Path": "/app/rstudio",
+                // "X-Forwarded-Proto": "https",
+                // "X-Forwarded-Host": "172.16.39.102",
             },
             body: new URLSearchParams({
                 persist: "0",
                 [csrf.attr("name")!]: csrf.attr("value")!,
                 appUri: "/",
-                clientPath: "/app/rstudio/auth-sign-in",
+                clientPath: "/auth-sign-in",
                 v: encrypt(payload, exp, mod),
-            }).toString(),
+            }),
         })
-
-        console.log(`##### get ${await response4.text()} #####`)
-        response4.headers.forEach((value, key) => console.log(key + ": " + value))
-        console.log("###########")
-        console.log(JSON.stringify(response4.headers.getSetCookie()))
-
-        for (const cookie of parser.parse(response4.headers.getSetCookie())) {
-            console.log(cookie.name + ": " + cookie.value)
-        }
 
         if (response4.headers.getSetCookie().length > 0) {
             break
         }
     }
-
-    // let response0 = await testFetch("/post", {
-    //     method: "POST",
-    //     headers: { "Content-Type": "application/json" },
-    //     body: JSON.stringify({
-    //         a: new URLSearchParams({
-    //             persist: "0",
-    //             [csrf.attr("name")!]: csrf.attr("value")!,
-    //             appUri: "/",
-    //             clientPath: "/app/rstudio/auth-sign-in",
-    //             v: encrypt(payload, exp, mod),
-    //         }).toString(),
-    //     }),
-    // })
-    //
-    // console.log(`##### get ${JSON.stringify(await response0.json())} #####`)
-    //
-    // response0 = await testFetch("/post", {
-    //     method: "POST",
-    //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    //     body: new URLSearchParams({
-    //         persist: "0",
-    //         [csrf.attr("name")!]: csrf.attr("value")!,
-    //         appUri: "/",
-    //         clientPath: "/app/rstudio/auth-sign-in",
-    //         v: encrypt(payload, exp, mod),
-    //     }).toString(),
-    // })
-    //
-    // console.log(`##### get ${JSON.stringify(await response0.json())} #####`)
-    //
-    // response0 = await testFetch("/post", {
-    //     method: "POST",
-    //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    //     body: new URLSearchParams({
-    //         persist: "0",
-    //         [csrf.attr("name")!]: csrf.attr("value")!,
-    //         appUri: "/",
-    //         clientPath: "/app/rstudio/auth-sign-in",
-    //         v: encrypt(payload, exp, mod),
-    //     }),
-    // })
-    //
-    // console.log(`##### get ${JSON.stringify(await response0.json())} #####`)
-    //
-    // response0 = await testFetch("/post", {
-    //     method: "POST",
-    //     body: new URLSearchParams({
-    //         persist: "0",
-    //         [csrf.attr("name")!]: csrf.attr("value")!,
-    //         appUri: "/",
-    //         clientPath: "/app/rstudio/auth-sign-in",
-    //         v: encrypt(payload, exp, mod),
-    //     }),
-    // })
-    //
-    // console.log(`##### get ${JSON.stringify(await response0.json())} #####`)
 
     return {
         title: "R Studio",
