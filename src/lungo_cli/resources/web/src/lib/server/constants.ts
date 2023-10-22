44ee2a16aa11bf861ad7a536ea3520ba7da9d2ab
raymond-u <36328498+raymond-u@users.noreply.github.com>
2023-10-22 20:42:38 +0800

    Update

--- src/lungo_cli/resources/web/src/lib/server/constants.ts
+++ src/lungo_cli/resources/web/src/lib/server/constants.ts
@@ -2,5 +2,7 @@ import { env } from "$env/dynamic/private"
 
 export const KETO_API_BASE_URL = env.KETO_API_BASE_URL
 export const KRATOS_API_BASE_URL = env.KRATOS_API_BASE_URL
+export const JUPYTERHUB_BASE_URL = env.JUPYTERHUB_BASE_URL
+export const JUPYTERHUB_PASSWORD = env.JUPYTERHUB_PASSWORD
 export const RSTUDIO_BASE_URL = env.RSTUDIO_BASE_URL
 export const RSTUDIO_PASSWORD = env.RSTUDIO_PASSWORD
---
+++ src/lungo_cli/resources/web/src/routes/(apps)/app/jupyterhub/+layout.server.ts
@@ -0,0 +1,48 @@
+import { load as loadHtmlString } from "cheerio"
+import type { Cookies } from "@sveltejs/kit"
+import { wrapFetch } from "$lib/server/api"
+import { JUPYTERHUB_BASE_URL, JUPYTERHUB_PASSWORD } from "$lib/server/constants"
+import type { User } from "$lib/types"
+
+export async function load({
+    cookies,
+    fetch,
+    parent,
+}: {
+    cookies: Cookies
+    fetch: typeof global.fetch
+    parent: () => Promise<{ userInfo: User["traits"] | undefined }>
+}) {
+    const wrappedFetch = wrapFetch({
+        fetch,
+        baseUrl: JUPYTERHUB_BASE_URL,
+        cookies,
+        cookiePath: "/app/jupyterhub",
+        credentials: "include",
+        ensureOk: true,
+    })
+
+    const response = await wrappedFetch("/app/jupyterhub/hub/login", { redirect: "manual" })
+
+    // If the user is already logged in, return
+    if (response.status == 302) {
+        return
+    }
+
+    const $ = loadHtmlString(await response.text())
+    const csrf = $("#login-main input[name=_xsrf]").attr("value")!
+
+    const { userInfo } = await parent()
+    const username = userInfo?.username ?? "anonymous"
+
+    await wrappedFetch("/app/jupyterhub/hub/login", {
+        method: "POST",
+        redirect: "manual",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams({
+            _xsrf: csrf,
+            username,
+            password: JUPYTERHUB_PASSWORD,
+        }),
+    })
+}
