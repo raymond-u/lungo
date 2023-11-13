0aaab5af3f627ec94fdc6c10310400a6a5536bb8
raymond-u <36328498+raymond-u@users.noreply.github.com>
2023-11-13 22:47:56 +0800

    Hide nav rail if there is nothing to display

--- src/lungo_cli/resources/web/src/routes/(apps)/+layout.svelte
+++ src/lungo_cli/resources/web/src/routes/(apps)/+layout.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
     import { NavRail } from "$lib/components"
+
+    export let data
 </script>
 
-<div class="flex flex-none">
-    <NavRail />
-</div>
+{#if data.apps.length > 0}
+    <div class="flex flex-none">
+        <NavRail />
+    </div>
+{/if}
 <div class="flex flex-1">
     <slot />
 </div>
