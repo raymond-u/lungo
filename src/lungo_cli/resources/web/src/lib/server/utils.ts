f0cb774a3097e38c4e4c082f29b5dbebdc101285
raymond-u <36328498+raymond-u@users.noreply.github.com>
2024-03-19 10:33:14 +0800

    chore: stub

--- src/lungo_cli/resources/web/src/lib/server/utils.ts
+++ src/lungo_cli/resources/web/src/lib/server/utils.ts
@@ -1,4 +1,6 @@
 import type { AppInfo } from "$lib/types"
 
-// @ts-expect-error stub
-export function getAllApps(): AppInfo[] {}
+// Stub
+export function getAllApps(): AppInfo[] {
+    return []
+}
